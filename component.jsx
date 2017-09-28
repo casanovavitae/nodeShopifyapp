fb6a6ea3871dc7d0782ea7b5316d6f6564f39cda
Marco Casanova <casanova.vitae@gmail.com>
2017-09-28 14:33:36 -0500

    Add react component

---
+++ component.jsx
@@ -0,0 +1,38 @@
+//const React = require('react');
+import React from 'react';
+import {render} from 'react-dom';
+import {Page, Card} from '@shopify/polaris';
+import {EmbeddedApp} from '@shopify/polaris/embedded';
+import {axios} from 'axios';
+
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {data: ''};
+
+    }
+    render(){
+        console.log("props",this.props);
+      return(
+        <Page title="Example application">
+            <Card sectioned>
+            This Insert the rest of your app here, including those components detailed below, which can now communicate with the Embedded App SDK.
+            </Card>
+        </Page>
+      )  
+    }
+
+    componentDidUpdate(){
+        axios.defaults.headers.common['X-Shopify-Access-Token'] = this.props.token;
+        axios.get('/admin/products.json')
+        .then(function (response) {
+            console.log('response',response);
+            this.setState({data: response});
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    }
+}
+module.exports = App;
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var shopifyAPI = require('shopify-node-api');
 var config = require('../config/config');
+var React = require('react');
+var ReactDomServer = require('react-dom/server') ;
+const Component = require('../component');
+
 
 router.get('/', function(req, res){
     
@@ -11,8 +15,12 @@ router.get('/', function(req, res){
     var Shopify = new shopifyAPI(config);
     
     Shopify.exchange_temporary_token(query_params, function(err, data){
-        shopify_hidden_at=data['access_token'];
-        res.render('index', { title: 'Shopify'+data['access_token'] });
+        //shopify_hidden_at=data['access_token'];
+        //res.render('index', { title: 'Shopify'+data['access_token'] });
+        var html = ReactDomServer.renderToString(
+            React.createElement(Component,{token: data['access_token']})
+          );
+        res.send(html);
     });
 
 });
