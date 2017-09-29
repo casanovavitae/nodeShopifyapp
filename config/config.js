b5449532d611336f3d6ff56847a432d1046522bf
Marco Casanova <casanova.vitae@gmail.com>
2017-09-29 10:53:19 -0500

    Add credentials

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 exports.shop = 'storetestdt';
-exports.shopify_api_key = 'e8ca9e97e58dd11ad9be2501755b0cee';
-exports.shopify_shared_secret = '44102f59c90daa0ea57071a7d67c47a0';
+exports.shopify_api_key = '4a4ba97f41a191c1d268d9b00acd9ecb';
+exports.shopify_shared_secret = '2b541cd3d10de361bb9b6439f4885d3a';
 exports.shopify_scope = 'write_products';
-exports.redirect_uri = 'https://nodeshopify.herokuapp.com/auth';
+exports.redirect_uri = 'https://rsnapp.herokuapp.com/auth';
 exports.nonce = '';
